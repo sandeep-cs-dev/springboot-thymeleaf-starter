80402c49aa8ca16857f339639cf3c07a65111a5b
sandeep-cs-dev <sandeeppatelsgsits@gmail.com>
2020-07-10 02:59:12 +0530

    base commit

---
+++ src/main/java/com/test/onlinetest/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.test.onlinetest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greeting(Model model) {
+        model.addAttribute("name","Bob");
+        return "hello";
+    }
+
+}
